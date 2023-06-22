66bd4d4ef17376648b0995980442300a148a3a5a
LimShuonle <srxmax161@gmail.com>
2023-06-22 17:54:57 +0800

    creating upload image files

---
+++ src/controllers/upload.controller.js
@@ -0,0 +1,58 @@
+import  express  from "express";
+import prisma from "../utils/prisma.js";
+import auth from "../middlewares/auth.js";
+import { validateImage } from "../validators/upload.js";
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const allImages = await prisma.image.findMany();
+  res.json(allImages);
+});
+
+router.post('/', auth, async (req, res) => {
+  const data = req.body;
+
+  const validationErrors = validateImage(data);
+
+
+  if (Object.keys(validationErrors).length !== 0) {
+    return res.status(400).send({
+      error: validationErrors,
+    });
+  }
+
+    const imageData = {
+        id: data.id,
+        file: data.file,
+        name: data.name,
+        price: data.price,
+        title: data.title,
+        description: data.description,
+        created_at: data.created_at,
+        userId: data.user.payload.id,
+    }
+
+    try {
+    const image = await prisma.image.create({
+        data: imageData,
+    });
+    return res.json(image)
+    } catch (err) {
+        return res.status(500).send({error: "Failed to create image"})
+    }
+});
+
+router.delete('/:id', auth, async (req, res) => {
+    const image = await prisma.image.findUnique({
+        where: {
+            id: req.params.id,
+        }
+    })
+
+    if (req.user.id != image.userId) {
+        return res.status(401).send({"error": "Unauthorized"})
+    }
+})
+
+export default router;
---
+++ src/validators/upload.js
@@ -0,0 +1,12 @@
+export function validateImage(data){
+    const validationErrors = {}
+    if (!data.title || typeof data.title !== 'string') {
+        validationErrors.title = 'Title is required'
+    }
+
+    if(!data.file || typeof data.file !== 'string'){
+        validationErrors.file = 'Image is required'
+    }
+    
+    return validationErrors
+}
